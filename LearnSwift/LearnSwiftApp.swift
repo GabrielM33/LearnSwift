7c2bd78d243e660f66e27e3c27526696abce0ac8
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-07-16 23:22:21 -0300

    Initial Commit

---
+++ LearnSwift/LearnSwiftApp.swift
@@ -0,0 +1,34 @@
+//
+//  LearnSwiftApp.swift
+//  LearnSwift
+//
+//  Created by Gabriel Moraes on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
---
+++ LearnSwiftTests/LearnSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftTests.swift
+//  LearnSwiftTests
+//
+//  Created by Gabriel Moraes on 16/07/24.
+//
+
+import Testing
+@testable import LearnSwift
+
+struct LearnSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
